1c2b679919bde3d07b9a830292fafef2da24dad1
karthik540 <36918151+karthik540@users.noreply.github.com>
2019-02-27 11:07:58 +0530

    Added youtube api thingy

--- code/samppy.py
+++ code/samppy.py
@@ -1,8 +1,7 @@
 import urllib.request
 from urllib.parse import quote_plus
 import json
-s = quote_plus(str(input("Enter artist name : ")))
-y  = quote_plus(str(input("Enter song name : ")))
+"""
 with urllib.request.urlopen("http://api.musixmatch.com/ws/1.1/track.search?q_artist="+s+"&page_size=3&page=1&s_track_rating=desc&q_track="+y+"&apikey=d9f9da53de4a90b828fcf767cbfccffe") as url:
     s = url.read()
     j_obj = json.loads(s)
@@ -11,13 +10,20 @@ def genre():
         print('Not a vaid track')
     else:
         print (j_obj['message']['body']['track_list'][0]['track']['primary_genres']['music_genre_list'][0]['music_genre']['music_genre_name'])
-genre()
 def get():
     if not j_obj['message']['body']['track_list'][0]['track']['primary_genres']['music_genre_list'][0]['music_genre']['music_genre_name']:
         print('Not a vaid track')
     else:
         print (j_obj['message']['body']['track_list'][0]['track']['album_name'])
-get()
+"""
+def getvideo():
+    l = quote_plus(str(input("Enter the song name :")))
+    with urllib.request.urlopen("https://www.googleapis.com/youtube/v3/search?part=id&q="+l+"&type=video&key=AIzaSyBtN6nKC7Jaai3hIWlumCQgrtkBZcmWq4U") as url:
+        p = url.read()
+        j_objs = json.loads(p)
+        link = j_objs['items'][0]['id']['videoId']
+        return link
+"""get()
 x = j_obj['message']['body']['track_list'][0]['track']['primary_genres']['music_genre_list'][0]['music_genre']['music_genre_name']
 genrelist = []
 genrelist.append(x)
@@ -30,4 +36,4 @@ for row in l:
         out.write('%s' % column)
     out.write('\n')
 out.close()
-print('File transfer is done from list to csv')
+print('File transfer is done from list to csv')"""
